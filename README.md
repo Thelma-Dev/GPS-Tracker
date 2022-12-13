530d9ab1b081b63aa49faa809484634a8ade1bd5
Thelma-Dev <osifoaisosa@gmail.com>
2022-12-13 09:50:56 -0600

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # GPS Tracker
-Tracking a location using Map API
+Tracking a location using geolaction API and Mapbox.
+
+# Demo
+Click [here](https://thelma-dev.github.io/gps-tracker/) to track your location.
--- assets/script/index.js
+++ assets/script/index.js
@@ -9,6 +9,7 @@ function select(selector, parent = document) {
     return parent.querySelector(selector);
 }
 
+const overlay = select('.overlay');
 
 mapboxgl.accessToken = 'pk.eyJ1IjoidGhlbG1hLWRldiIsImEiOiJjbGJncnJqc2wwaXhjM29xd2liMXYzbmE4In0.c2LzFGTr8v0YUQlSfSe3mQ';
 
