e69110db38ce966fa6d7858a8c25a1a264a8fd26
Thelma-Dev <osifoaisosa@gmail.com>
2022-12-13 09:06:18 -0600

    Create files

---
+++ assets/script/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Utility functions
+function onEvent(event, selector, callback) {
+    return selector.addEventListener(event, callback);
+}
+
+function select(selector, parent = document) {
+    return parent.querySelector(selector);
+}
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidGhlbG1hLWRldiIsImEiOiJjbGJncnJqc2wwaXhjM29xd2liMXYzbmE4In0.c2LzFGTr8v0YUQlSfSe3mQ';
+
+
